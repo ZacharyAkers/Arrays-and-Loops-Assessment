e20a3126f8438c883dba56ca73f4a60e6ad08cb3
ZacharyAkers <user@Users-MacBook-Air.local>
2017-06-08 20:17:06 -0400

    resumit

--- main.js
+++ main.js
@@ -1,105 +1,115 @@
 // 1.
 // Without logging the values, list the value of `sports` and `total`
-var sports = ['soccer', 'baseball'];
-var total = sports.push('football', 'swimming');
+var sports = [‘soccer’, ‘baseball’];
+var total = sports.push(‘football’, ‘swimming’);
 // A:
-// sports = ["0", "1"]
-// total = ["0", "1", "2", "3"]
-
-
+sports = <‘soccer’,‘baseball’>
+ total = <‘soccer’,‘baseball’,‘football’,‘swimming’>
 // 2.
 // Using the `strings` array, wrte a function `longestString()`
 // that accepts an array argument and returns the longest string in the array
-var strings = ['this','is','a','collection','of','words'];
-// A:
-function longestString(array) {
-    for (var i = 0; i < array.length; i++) {
-        if ((typeof array[i] === "string") && (array[i].length > strings.length)) {
-            result = array[i];
-        }
-    }
-    return result;
-}
-
-
-console.assert(longestString(strings) === 'collection', {"message": "longestString should return 'collection'"});
-
-
+var strings = [‘this’,‘is’,‘a’,‘collection’,‘of’,‘words’];
+ // A:
+ var longestString =function(array){
+   let longestNum = “”
+   for (let i = 0; i < array.length; i++){
+     if (array[i].length > longestNum.length){
+       longestNum = array[i];
+     }
+   }
+   return longestNum;
+ }
+console.assert(longestString(strings) === ‘collection’, {“message”: “longestString should return ‘collection’“});
 // Use the `numbers` array for questions 3 - 8.
 var numbers = [1,12,4,18,9,7,11,3,101,5,6];
-
-
 // 3.
 // Write a function `smallestNumber()` that accepts an array
 // and returns the smallest number in the array.
 // A:
-function smallestNumber(array) {
-    return Math.min.apply(numbers, array);
-}
-
-console.assert(smallestNumber(numbers) === 1, {"message": "smallestNumber should return 1"});
-
-
+var numbers = [1,12,4,18,9,7,11,3,101,5,6]
+function smallestNumber(array)
+  let smallestNum = 102;{
+    for(let i = 0; i < array.length; i++){
+      if(array[i] < smallestNum){
+        smallestNum = array[i];
+      }
+    }
+    return smallestNum
+  }
+console.assert(smallestNumber(numbers) === 1, {“message”: “smallestNumber should return 1"});
 // 4.
 // Write a function `getEvens()` that accepts an array
 // and returns only the even numbers in the array.
 // A:
-function getEvens(array) {
-    if(numbers % 2 == 0)
-{
-  return ('is even');
+var numbers = [1,12,4,18,9,7,11,3,101,5,6]
+function getEvens(array){
+  var evenNumberArray = [];
+  for(let i = 0; i < array.length i++){
+    if numbers[i] %2 == 0){
+      evenNumberArray.push(numbers [i]);
+    }
+  }
+  return evenNumberArray
 }
-
-console.assert(getEvens(numbers).toString() === '12,4,18,6', {'message': 'getEvens should return "12,4,18,6"'});
-
-
+console.assert(getEvens(numbers).toString() === ‘12,4,18,6’, {‘message’: ‘getEvens should return “12,4,18,6”‘});
 // 5.
 // Write a function that accepts an array argument
 // and returns the array reversed
 // Hint: When looping over the array, start at the last index
 // and decrement the iterator to zero
 // A:
-function reverseArr(array) {
-    var rev = array;
-    for(var i = array.length-1; i > 0; i--) {
-        rev.push(array[i]);
-    }
-    return rev;
+var numbers = [1,12,4,18,9,7,11,3,101,5,6]
+function reverseArray(array){
+  var numbersInReverseArray =[];{
+  for(let i =array.length; i >= 0; i--){
+    numbersInReverseArray.push(array[i]);
+  }
 }
-
-console.assert(arrayReverser(numbers).toString() === '6,5,101,3,11,7,9,18,4,12,1', {'message': 'arrayReverser should return "6,5,101,3,11,7,9,18,4,12,1"'});
-
-
+return numbersInReverseArray;
+}
+console.assert(arrayReverser(numbers).toString() === ‘6,5,101,3,11,7,9,18,4,12,1’, {‘message’: ‘arrayReverser should return “6,5,101,3,11,7,9,18,4,12,1"‘});
 // 6.
 // Write a function that accepts an array argument
 // and returns the sum of all of the numbers in the array
 // A:
-
-
-console.assert(sumArrayOfNumbers(numbers) === 177, {'message': 'sumArrayOfNumbers should return 177'});
-
-
+var numbers = [1,12,4,18,9,7,11,3,101,5,6]
+function sumAll(array){
+  let counter = 0;
+  for(let i = 0; i < array.length; i++){
+    counter += array[i];
+  }
+  return counter;
+}
+console.assert(sumArrayOfNumbers(numbers) === 177, {‘message’: ‘sumArrayOfNumbers should return 177’});
 // 7.
 // Write a function that accepts an array argument
 // and returns an array of only the numbers greater than 10
 // A:
-function biggerThanTen(array) {
-  return function(numbers, index, array) {
-    return (numbers >= 10);
+var numbers = [1,12,4,18,9,7,11,3,101,5,6]
+function numbersOver10(array){
+  let overTenArray =[];
+  for(let i = 0; i < array.length; i++){
+    if(array[i] > 10){
+    }
   }
-}
-
-console.assert(numbersOver10(numbers).toString() === "12,18,11,101", {'message': 'numbersOver10 should return "12,18,11,101"'});
-
-
+    return overTenArray;
+  }
+console.assert(numbersOver10(numbers).toString() === “12,18,11,101”, {‘message’: ‘numbersOver10 should return “12,18,11,101"‘});
 // 8.
 // Write a function that accepts both an array and number argument
 // and returns an array of only the numbers greater than the number passed to the function
 // A:
-
-
-console.assert(numbersOverX(numbers, 15).toString() === "18,101", {'message': 'numbersOverX should return "18,101"'});
-
+var numbers = [1,12,4,18,9,7,11,3,101,5,6]
+function numbersOverX(array, number){
+  let numOverXArray = [];
+  for (let i = 0; i < array.length; i++){
+    if(array [i] > number){
+        numOverXArray.push(array[i]);
+    }
+  }
+  return numOverXArray;
+}
+console.assert(numbersOverX(numbers, 15).toString() === “18,101”, {‘message’: ‘numbersOverX should return “18,101"‘});
 // 9.
 // Write a function `joinArrays()` that takes an array of arrays,
 // and returns a single array with the contents of the second array
@@ -107,34 +117,28 @@ console.assert(numbersOverX(numbers, 15).toString() === "18,101", {'message': 'n
 var numbers = [1,12,4,18,9,7,11,3,101,5,6];
 var numbersTwo = [33,56,72,2,5,66,90,21,42];
 // A:
-function joinArray(arr, arr2) {
-    arr.push.apply(arr, arr2);
-}
-
-console.assert(joinArrays([numbers, numbersTwo]).toString() === '1,12,4,18,9,7,11,3,101,5,6,33,56,72,2,5,66,90,21,42', {'message': 'joinArrays should return "1,12,4,18,9,7,11,3,101,5,6,33,56,72,2,5,66,90,21,42"'});
-
-
+console.assert(joinArrays([numbers, numbersTwo]).toString() === ‘1,12,4,18,9,7,11,3,101,5,6,33,56,72,2,5,66,90,21,42’, {‘message’: ‘joinArrays should return “1,12,4,18,9,7,11,3,101,5,6,33,56,72,2,5,66,90,21,42”‘});
 // 10.
 // Using the `instructors` array, comment the value of each variable below
 var instructors = [
-    ['JD','JavaScript'],
-    ['Tim','JavaScript'],
-    ['Brit','Ruby'],
-    ['Joe','iOS'],
-    ['Dan','JavaScript'],
-    ['Will','JavaScript'],
-    ['Calvin','JavaScript'],
-    ['James','Ruby']
+    [‘JD’,‘JavaScript’],
+    [‘Tim’,‘JavaScript’],
+    [‘Brit’,‘Ruby’],
+    [‘Joe’,‘iOS’],
+    [‘Dan’,‘JavaScript’],
+    [‘Will’,‘JavaScript’],
+    [‘Calvin’,‘JavaScript’],
+    [‘James’,‘Ruby’]
 ];
-
 var instructorNameDiscipline = instructors[5];
-// greenvIlleInstructor = <'Will', 'JavaScript'>
-
+// greenvIlleInstructor = <your answer>
+[‘Will’,‘JavaScript’]
 var instructorOne = instructors[4][0];
-// instructorOne = <'Dan'>
-
+// instructorOne = <your answer>
+[‘Dan’]
 var instructorTwo = instructors[0][1];
-// instructorTwo = <'JavaScript'>
-
+// instructorTwo = <your answer>
+[‘JavaScript’]
 var instructorThree = instructors[2][0];
-// instructorThree = <'Brit'>
+// instructorThree = <your answer>
+[‘Brit’]
